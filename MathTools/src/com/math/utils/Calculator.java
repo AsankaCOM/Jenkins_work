0f6ee4df00180d61a27d385baf68da3adef94521
AsankaCOM <per.asanka@gmail.com>
2015-02-26 21:40:00 -0700

    commit

--- MathTools/src/com/math/utils/Calculator.java
+++ MathTools/src/com/math/utils/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello jenkins");
-		System.out.println(" 1 + 2 = " + add(1,2));
+		System.out.println(" 1 + 2 = " + add(1,2)); 
 	}
 	
 	public static int add (int a, int b) {
